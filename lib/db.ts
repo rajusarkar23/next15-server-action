9452b11d23996ad84c9017e2b632d293d3b26a65
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-11-25 17:27:22 +0530

    comment mongo uri

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-const MONGO_URI = process.env.MONGO_URI
+// const MONGO_URI = process.env.MONGO_URI
 
 // if (!MONGO_URI) {
 //     throw new Error("Invalid mongo uri.")

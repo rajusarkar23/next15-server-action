83688b7d90213b9c311bb9f6ba9324f1f8718bd5
rsarkarr <97837402+rsarkarr@users.noreply.github.com>
2024-11-26 17:33:23 +0530

    change mongo uri call method

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,17 @@
 import mongoose from "mongoose"
 
-// const MONGO_URI = process.env.MONGO_URI
+const MONGO_URI = process.env.MONGO_URI
 
-// if (!MONGO_URI) {
-//     throw new Error("Invalid mongo uri.")
-// }
+if (!MONGO_URI) {
+    throw new Error("Invalid mongo uri.")
+}
 
 export async function connectToDB(){
     if (mongoose.connection.readyState === 0) {
         await mongoose.connect(`${process.env.MONGO_URI}`)
         console.log("db connected!!!");
+        console.log(process.env.MONGO_URI);
+        
         
     }
 }
